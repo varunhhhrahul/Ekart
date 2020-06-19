41b6c48d7e5bcd552ec6ea95e4d2e53528de4f5a
varunhhhrahul <varunhhhrahul@gmail.com>
2020-06-19 14:52:14 +0530

    first commit

---
+++ views/admin/products/new.js
@@ -0,0 +1,40 @@
+const layout = require('../layout');
+const { getError } = require('../../helpers');
+
+module.exports = ({ errors }) => {
+	return layout({
+		content: `
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            <div class="field">
+              <label class="label">Description</label>
+              <textarea class="textarea" placeholder="Description" name="description" ></textarea>
+              <p class="help is-danger">${getError(errors, 'description')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    `
+	});
+};
