602eafc7f1de3bff4735a3b3a76f5ddb5565f554
Pranav Tanpure <pranav280300@gmail.com>
2020-06-19 23:19:01 +0530

    m yf irst commit

--- views/layout.js
+++ views/layout.js
@@ -5,7 +5,7 @@ module.exports = ({ content }) => {
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ekart</title>
+        <title>Ekart2</title>
         
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
         <link href="/css/main.css" rel="stylesheet">
